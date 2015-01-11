c1ee1a52c94f5db9f56d7ce3a09da291907ee790
MattFromGer <matzemink@gmail.com>
2015-01-11 22:09:33 +0100

    Initial commit

---
+++ app/src/main/java/com/androiddev/mattfromger/smartquickcircle/MainClass.java
@@ -0,0 +1,52 @@
+/**
+ * Created by MattFromGer on 11/01/15.
+ */
+
+package com.androiddev.mattfromger.smartquickcircle;
+
+import android.util.Log;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import android.graphics.Color;
+import android.widget.TextView;
+
+public class MainClass implements IXposedHookLoadPackage{
+
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        Log.v("com.androiddev.mattfromger.smartquickcircle","Loaded");
+/*
+        findAndHookMethod("com.lge.clock.quickcover.QuickCoverFactory", lpparam.classLoader, "getQuickCover", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                Log.v("com.androiddev.mattfromger.smartquickcircle","lolololololololol");
+                XposedBridge.log("lolololololololololololol");
+            }
+        });*/
+
+
+        findAndHookMethod("com.android.keyguard", lpparam.classLoader, "showQuickCoverWindow", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                Log.v("com.androiddev.mattfromger.smartquickcircle","lolololololololol");
+                XposedBridge.log("lolololololololololololol");
+            }
+        });
+        /*
+        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                TextView tv = (TextView) param.thisObject;
+                String text = tv.getText().toString();
+                tv.setText(text + " :)");
+                tv.setTextColor(Color.RED);
+            }
+        });*/
+    }
+}
