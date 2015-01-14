ef827a700ed97ba67afb04e562439db9fa5be7cd
MattFromGer <matzemink@gmail.com>
2015-01-14 19:32:08 +0100

    Initial commit

--- app/src/main/java/com/androiddev/mattfromger/smartquickcircle/MainClass.java
+++ app/src/main/java/com/androiddev/mattfromger/smartquickcircle/MainClass.java
@@ -18,7 +18,7 @@ public class MainClass implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 
         XposedBridge.log("Loaded app: " + lpparam.packageName);
-        Log.v("com.androiddev.mattfromger.smartquickcircle","test1234");
+        Log.v("com.androiddev.mattfromger.smartquickcircle","test12345");
 
         //findAndHookMethod("com.android.keyguard", lpparam.classLoader, "showQuickCoverWindow", new XC_MethodHook() {
         findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
